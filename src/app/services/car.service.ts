edf5ac31fa4a404f598e1e879ea3442b9e128673
soyma20 <slaviksoyma2014@gmail.com>
2022-11-07 15:56:07 +0200

    feat: add update

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -20,6 +20,17 @@ export class CarService {
     return this.storageService.getCars();
   }
 
+  updateCar(car: ICar): void {
+    const cars = this.storageService.getCars();
+    console.log(cars)
+    cars.map((value) => {
+      if (value.id === car.id) value = car
+      console.log(value)
+    });
+    console.log(cars)
+    this.storageService.saveCars(cars)
+  }
+
 
 
 }
