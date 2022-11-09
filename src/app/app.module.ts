4ab696b9501ea11e3b9cfd34881a538c6c4bfa9a
soyma20 <slaviksoyma2014@gmail.com>
2022-11-09 21:01:05 +0200

    feat: add declarations and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {CarsModule} from "./modules";
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {HeaderComponent} from './components/header/header.component';
+import {BannerComponent} from './components/banner/banner.component';
+import {HideAfterDirective} from "./directives/hide-after.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    BannerComponent,
+    HideAfterDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    CarsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
